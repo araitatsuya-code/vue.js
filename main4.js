d449a479347b4f49dc876db15fe6e4204fb22bee
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-03-10 23:03:05 +0900

    JSONP

---
+++ main4.js
@@ -0,0 +1,16 @@
+//JSONPのURL
+var url = products.js
+  // 非同期通信でJSONを読み込む
+  $.ajax({
+    url : 'products.json',  // 通信先URL
+    type: 'GET',            // 使用するHTTPメソッド（デフォルトがGETなので省略可能）
+    dataType: 'jsonp',      // レスポンスのデータタイプ
+    jsonp: 'callback',      // クエリパラメータの名前
+    jsonpCallback: 'products' // コールバック関数の名前
+  })
+  .done(function(data, textStatus, jqXHR) {
+    console.log(data);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log('通信が失敗しました');
+  });
